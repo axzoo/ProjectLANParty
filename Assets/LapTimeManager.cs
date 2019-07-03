0e400808b1454f4a37947104bd15d72b5868b96f
JINazxo筆記本 <43165736+axzoo@users.noreply.github.com>
2019-07-03 16:35:19 +0800

    update. still building.

---
+++ Assets/LapTimeManager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+public class LapTimeManager : MonoBehaviour
+{
+    public static int MinuteCount;
+    public static int SecondCount;
+    public static float MilliCount;
+    public static string MillDisplay;
+
+    public GameObject MinuteBox;
+    public GameObject SecondBox;
+    public GameObject MillBox;
+
+    private void Update()
+    {
+        MilliCount += Time.deltaTime * 10;
+        MillDisplay = MilliCount.ToString("F0");
+        MillBox.GetComponent<Text>().text = "" + MillDisplay;
+
+        if (MilliCount >= 10)
+        {
+            MilliCount = 0;
+            SecondCount += 1;
+        }
+
+        if (SecondCount <= 9)
+        {
+            SecondBox.GetComponent<Text>().text = "0" + SecondCount + ".";
+        }
+        else
+        {
+            SecondBox.GetComponent<Text>().text = "" + SecondCount + ".";
+        }
+
+        if (SecondCount >= 60)
+        {
+            SecondCount = 0;
+            MinuteCount += 1;
+        }
+
+        if ( MinuteCount <= 9)
+        {
+            MinuteBox.GetComponent<Text>().text = "0" + MinuteCount + ":";
+        }
+        else
+        {
+            MinuteBox.GetComponent<Text>().text = "" + MinuteCount + ":";
+        }
+
+    }
+}
